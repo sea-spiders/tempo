8799735346f42cc80fe245f3002ca6b1839daa65
daniel-doody <daniel.doody@ymail.com>
2023-01-05 08:45:05 -0500

    redux folder removed, references to redux in Client folder removed.

---
+++ src/client/Components/cards/Flashcard/Flashcard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './flashcard.module.css';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const Card = ({ data }) => {
+  const { back, difficulty, front, hints, scheduled, title, user_id, _id } =
+    data;
+  const navigate = useNavigate();
+  return (
+    <>
+      <Link to={`/flashcard/${_id}`}>
+        <div className={`${styles.backgroundGrey}`}>
+          <div className={`${styles.title}`}>{title ?? 'no title'}</div>
+          <div className={`${styles.front}`}>{front ?? 'no front'}</div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default Card;
