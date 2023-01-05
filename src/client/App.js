56bf841e26ce8feeea4443db388acc2e4d1b93b7
daniel-doody <daniel.doody@ymail.com>
2023-01-05 09:05:08 -0500

    fixed redux-related bugs in navbar, app.js, login-page, user-profile, index

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,5 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { setEmail, setPicture } from './Redux/slices/userSlice';
 import LandingPage from './Components/pages/LandingPage/LandingPage';
 import Home from './Components/pages/Home/Home';
 import Navbar from './Components/global/Navbar/Navbar';
@@ -16,46 +13,30 @@ const App = () => {
     home: '/home',
   };
 
-  // On first render, get user data
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const response = axios({
-      method: 'get',
-      withCredentials: true,
-      url: '/auth/user',
-    }).then((res) => {
-      if (res.data) {
-        dispatch(setEmail(res.data.email));
-        dispatch(setPicture(res.data.picture));
-      }
-    });
-  }, []);
-  const email = useSelector((state) => state.user.email);
-
   return (
     <>
-      {!email ? (
+      {/* {!email ? (
         <LandingPage />
-      ) : (
-        <BrowserRouter>
-            {/* Component 'Navbar' must be placed within browser router so that navbar links work */}
-          {/* <Navbar leftItems={leftItems} /> */}
-            {/* {location.pathname !== '/' && <Navbar />} */}
-            <Navbar leftItems={leftItems} />
+      ) : ( */}
+      <BrowserRouter>
+        {/* Component 'Navbar' must be placed within browser router so that navbar links work */}
+        {/* <Navbar leftItems={leftItems} /> */}
+        {location.pathname !== '/' && <Navbar leftItems={leftItems} />}
+        {/* <Navbar leftItems={leftItems} /> */}
 
-          <div>
-            <Routes>
-                <Route exact path="/" element={<LandingPage />} />
-                {/* Home: shows DecksContainer */}
-              <Route exact path="/home" element={<Home />} />
-              <Route exact path="/createDeck" element={<CreateDeck />} />
-              <Route exact path="/createCard" element={<CreateCard />} />
-              <Route exact path="/deck-page" element={<DeckPage />} />
-              <Route exact path="/flashcard/:id" element={<FlashCard />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      )}
+        <div>
+          <Routes>
+            <Route exact path='/' element={<LandingPage />} />
+            {/* Home: shows DecksContainer */}
+            <Route exact path='/home' element={<Home />} />
+            <Route exact path='/createDeck' element={<CreateDeck />} />
+            <Route exact path='/createCard' element={<CreateCard />} />
+            <Route exact path='/deck-page' element={<DeckPage />} />
+            <Route exact path='/flashcard/:id' element={<FlashCard />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      {/* )} */}
     </>
   );
 };
--- src/client/Components/global/Navbar/UserProfile.jsx
+++ src/client/Components/global/Navbar/UserProfile.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import styles from './userProfile.module.css'
+import styles from './userProfile.module.css';
 
 const UserProfile = () => {
-
-  const userEmail = useSelector((state) => state.user.email);
-  const userPicture = useSelector((state) => state.user.picture);
-
   return (
     <>
       <div className={styles.userProfileContainer}>
-        <span className={styles.userEmail}>Welcome, {userEmail}</span>
-        <img src={userPicture} alt="User" className={styles.userPicture} />
+        <span className={styles.userEmail}>Welcome, PLACE USERNAME HERE</span>
+        {/* <img src={userPicture} alt="User" className={styles.userPicture} /> */}
       </div>
     </>
   );
